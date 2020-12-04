3618ba14a97d6dea457b3d8ffefead7fb161837e
jorgebarbosat <62555995+jorgebarbosat@users.noreply.github.com>
2020-12-04 12:22:05 -0300

    .

--- componentes/home.js
+++ componentes/home.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { View, Image, Text, Button } from 'react-native';
+import { View, Image, Text, Button, StyleSheet, SafeAreaView } from 'react-native';
+
+const Separator = () => (
+    <View style={styles.separator} />
+  );
 
 const HomeApp = ({ navigation }) => {
     return(
-        <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+        <SafeAreaView style={styles.container}>
+        <View style={{flex: 0, justifyContent: 'center', alignItems: 'center'}}>
             <Image 
                 style={{width: 100, height: 70, marginTop: 30}}
                 source={require('../imagens/logo.jpg')}
@@ -16,27 +21,61 @@ const HomeApp = ({ navigation }) => {
                 onPress={() => navigation.navigate('Blusas')}
             />
 
-            <Button 
+        </View>
+        <Separator/>
+     
+        <View style={{flex: 0, justifyContent: 'center', alignItems: 'center'}} >
+
+        <Button 
                 title="Calças/Bermudas"
                 onPress={() => navigation.navigate('Calças/Bermudas')}
             />
 
-            <Button 
+        </View>
+        <Separator/>
+        <View style={{flex: 0, justifyContent: 'center', alignItems: 'center'}}>
+
+        <Button 
                 title="Tênis"
                 onPress={() => navigation.navigate('Tênis')}
             />
 
-            <Button 
+        </View>
+        <Separator/>
+        <View style={{flex: 0, justifyContent: 'center', alignItems: 'center'}}>
+
+        <Button 
                 title="Bonés"
                 onPress={() => navigation.navigate('Bonés')}
             />
 
-            <Button 
+        </View>
+        <Separator/>
+        <View style={{flex: 0, justifyContent: 'center', alignItems: 'center'}}>
+
+        <Button 
                 title="Sobre"
                 onPress={() => navigation.navigate('Sobre')}
             />
+            
         </View>
+        <Separator/>
+    </SafeAreaView>
     );
 }   
+
+const styles = StyleSheet.create({
+    separator: {
+        marginVertical: 8,
+        borderBottomColor: '#737373',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+      },
+      container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginHorizontal: 20,
+      }
+    });
+    
 
 export default HomeApp;
